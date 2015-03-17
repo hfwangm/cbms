14cf76d11ec307cc7c84f2b5600897df9ca5f635
Administrator <71985506@163.com>
2015-03-17 17:09:04 +0800

    20150317

--- src/main/java/com/cbms/commons/mapper/JsonMapper.java
+++ src/main/java/com/cbms/commons/mapper/JsonMapper.java
@@ -97,7 +97,7 @@ public class JsonMapper {
     /**
      * 反序列化复杂Collection如List<Bean>, 先使用createCollectionType()或contructMapType()构造类型, 然后调用本函数.
      *
-     * @see #createCollectionType(Class, Class...)
+     * *@see createCollectionType(Class, Class...)
      */
     public <T> T fromJson(String jsonString, JavaType javaType) {
         if (StringUtils.isEmpty(jsonString)) {
